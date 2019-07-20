6d50145eea1ab9a6274a8fead1486e19d59e5dc9
JakubWidaEBiznes <jakub.wida.box@gmail.com>
2019-07-20 15:46:20 +0200

    add timer util

---
+++ Code/Utils/Timer.py
@@ -0,0 +1,20 @@
+import time
+
+class Timer:
+
+	def __init__(self):
+		self.timer_dict={}
+
+	def start_timer(self,name):
+		self.timer_dict[name]=[time.time(),None,None]
+
+	def stop_timer(self,name,do_print=True):
+		curr_time = time.time()
+		diff_time = curr_time - self.timer_dict[name][0]
+		self.timer_dict[name][1] = curr_time
+		self.timer_dict[name][2] = diff_time
+		if do_print:
+			print(f'TIMER: {name:20s} {diff_time:.20f}')
+
+	def get_timers(self):
+		return self.timer_dict
